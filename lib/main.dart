bcd02a46f3072f08b4b205d2ba0bb7b5e346abcd
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-12 16:34:39 -0400

    kkk

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dado app'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
